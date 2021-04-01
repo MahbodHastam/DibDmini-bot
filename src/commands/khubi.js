64cc510dec59640c7e69fab7f1effb33bad08b1e
Mahbod <mahbod4782@gmail.com>
2021-04-01 20:03:46 +0430

    initial comm

---
+++ src/commands/khubi.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'خوبی',
+  description: 'خیخیخی',
+  execute(message, args) {
+    message.reply(`چرا که نه :)`);
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { PREFIX, BOT_TOKEN, SRC_DIR } = require('./config');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.login(BOT_TOKEN);
+const fs = require('fs');
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync(`./${SRC_DIR}commands`)
+  /* .filter(file => file.endsWith('js')) */;
+let args;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  client.user.setPresence({
+    status: 'idle',
+    activity: { name: 'دیبی', type: 'LISTENING' },
+  });
+});
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on('message', message => {
+  if (message.content === PREFIX) {
+    message.reply(`بلی`);
+  }
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+  args = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply(`Server error! Please tell the developer.`);
+  }
+});
